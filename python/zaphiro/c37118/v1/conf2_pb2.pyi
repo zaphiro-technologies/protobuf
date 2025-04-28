99f71ab0b9a4de578337567b3602d56c46775cb5
tejo <m.parmi@gmail.com>
2025-04-28 21:34:19 +0200

    Add config 3 compatibility in config proto.

--- python/zaphiro/c37118/v1/conf2_pb2.pyi
+++ python/zaphiro/c37118/v1/conf2_pb2.pyi
@@ -1,9 +1,27 @@
 from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class FrameType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    FRAME_TYPE_UNSPECIFIED: _ClassVar[FrameType]
+    FRAME_TYPE_DATA: _ClassVar[FrameType]
+    FRAME_TYPE_HEADER: _ClassVar[FrameType]
+    FRAME_TYPE_CONFIG_1: _ClassVar[FrameType]
+    FRAME_TYPE_CONFIG_2: _ClassVar[FrameType]
+    FRAME_TYPE_COMMAND: _ClassVar[FrameType]
+    FRAME_TYPE_CONFIG_3: _ClassVar[FrameType]
+FRAME_TYPE_UNSPECIFIED: FrameType
+FRAME_TYPE_DATA: FrameType
+FRAME_TYPE_HEADER: FrameType
+FRAME_TYPE_CONFIG_1: FrameType
+FRAME_TYPE_CONFIG_2: FrameType
+FRAME_TYPE_COMMAND: FrameType
+FRAME_TYPE_CONFIG_3: FrameType
 
 class Conf2Frame(_message.Message):
     __slots__ = ("header", "configs", "DATA_RATE")
@@ -34,7 +52,7 @@ class Conf2Header(_message.Message):
     def __init__(self, SYNC: _Optional[int] = ..., FRAMESIZE: _Optional[int] = ..., IDCODE: _Optional[int] = ..., SOC: _Optional[int] = ..., FRACSEC: _Optional[int] = ..., TIME_BASE: _Optional[int] = ..., NUM_PMU: _Optional[int] = ...) -> None: ...
 
 class Config(_message.Message):
-    __slots__ = ("STN", "IDCODE", "FORMAT", "PHNMR", "ANNMR", "DGNMR", "CHNAM", "PHUNIT", "ANUNIT", "DIGUNIT", "FNOM", "CFGCNT")
+    __slots__ = ("STN", "IDCODE", "FORMAT", "PHNMR", "ANNMR", "DGNMR", "CHNAM", "PHUNIT", "ANUNIT", "DIGUNIT", "FNOM", "CFGCNT", "frame_type", "version", "G_PMU_ID", "PHSCALE", "ANSCALE", "PMU_LAT", "PMU_LON", "PMU_ELEV", "SVC_CLASS", "WINDOW", "GRP_DLY")
     STN_FIELD_NUMBER: _ClassVar[int]
     IDCODE_FIELD_NUMBER: _ClassVar[int]
     FORMAT_FIELD_NUMBER: _ClassVar[int]
@@ -47,6 +65,17 @@ class Config(_message.Message):
     DIGUNIT_FIELD_NUMBER: _ClassVar[int]
     FNOM_FIELD_NUMBER: _ClassVar[int]
     CFGCNT_FIELD_NUMBER: _ClassVar[int]
+    FRAME_TYPE_FIELD_NUMBER: _ClassVar[int]
+    VERSION_FIELD_NUMBER: _ClassVar[int]
+    G_PMU_ID_FIELD_NUMBER: _ClassVar[int]
+    PHSCALE_FIELD_NUMBER: _ClassVar[int]
+    ANSCALE_FIELD_NUMBER: _ClassVar[int]
+    PMU_LAT_FIELD_NUMBER: _ClassVar[int]
+    PMU_LON_FIELD_NUMBER: _ClassVar[int]
+    PMU_ELEV_FIELD_NUMBER: _ClassVar[int]
+    SVC_CLASS_FIELD_NUMBER: _ClassVar[int]
+    WINDOW_FIELD_NUMBER: _ClassVar[int]
+    GRP_DLY_FIELD_NUMBER: _ClassVar[int]
     STN: str
     IDCODE: int
     FORMAT: int
@@ -59,4 +88,33 @@ class Config(_message.Message):
     DIGUNIT: _containers.RepeatedScalarFieldContainer[int]
     FNOM: int
     CFGCNT: int
-    def __init__(self, STN: _Optional[str] = ..., IDCODE: _Optional[int] = ..., FORMAT: _Optional[int] = ..., PHNMR: _Optional[int] = ..., ANNMR: _Optional[int] = ..., DGNMR: _Optional[int] = ..., CHNAM: _Optional[str] = ..., PHUNIT: _Optional[_Iterable[int]] = ..., ANUNIT: _Optional[_Iterable[int]] = ..., DIGUNIT: _Optional[_Iterable[int]] = ..., FNOM: _Optional[int] = ..., CFGCNT: _Optional[int] = ...) -> None: ...
+    frame_type: FrameType
+    version: int
+    G_PMU_ID: bytes
+    PHSCALE: _containers.RepeatedCompositeFieldContainer[PhasorScaling]
+    ANSCALE: _containers.RepeatedCompositeFieldContainer[AnalogScaling]
+    PMU_LAT: float
+    PMU_LON: float
+    PMU_ELEV: float
+    SVC_CLASS: str
+    WINDOW: int
+    GRP_DLY: int
+    def __init__(self, STN: _Optional[str] = ..., IDCODE: _Optional[int] = ..., FORMAT: _Optional[int] = ..., PHNMR: _Optional[int] = ..., ANNMR: _Optional[int] = ..., DGNMR: _Optional[int] = ..., CHNAM: _Optional[str] = ..., PHUNIT: _Optional[_Iterable[int]] = ..., ANUNIT: _Optional[_Iterable[int]] = ..., DIGUNIT: _Optional[_Iterable[int]] = ..., FNOM: _Optional[int] = ..., CFGCNT: _Optional[int] = ..., frame_type: _Optional[_Union[FrameType, str]] = ..., version: _Optional[int] = ..., G_PMU_ID: _Optional[bytes] = ..., PHSCALE: _Optional[_Iterable[_Union[PhasorScaling, _Mapping]]] = ..., ANSCALE: _Optional[_Iterable[_Union[AnalogScaling, _Mapping]]] = ..., PMU_LAT: _Optional[float] = ..., PMU_LON: _Optional[float] = ..., PMU_ELEV: _Optional[float] = ..., SVC_CLASS: _Optional[str] = ..., WINDOW: _Optional[int] = ..., GRP_DLY: _Optional[int] = ...) -> None: ...
+
+class PhasorScaling(_message.Message):
+    __slots__ = ("scale", "offset", "flags")
+    SCALE_FIELD_NUMBER: _ClassVar[int]
+    OFFSET_FIELD_NUMBER: _ClassVar[int]
+    FLAGS_FIELD_NUMBER: _ClassVar[int]
+    scale: int
+    offset: int
+    flags: int
+    def __init__(self, scale: _Optional[int] = ..., offset: _Optional[int] = ..., flags: _Optional[int] = ...) -> None: ...
+
+class AnalogScaling(_message.Message):
+    __slots__ = ("scale", "offset")
+    SCALE_FIELD_NUMBER: _ClassVar[int]
+    OFFSET_FIELD_NUMBER: _ClassVar[int]
+    scale: int
+    offset: int
+    def __init__(self, scale: _Optional[int] = ..., offset: _Optional[int] = ...) -> None: ...
